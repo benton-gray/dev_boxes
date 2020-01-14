32d02f2b5acd951f1d5a8f3b6bf61583f1991a97
benton-gray <benton.gray@outlook.com>
2020-01-13 21:36:54 -0700

    Adding docker installation steps for Centos8

--- centos/init.bash
+++ centos/init.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Init Provisioning Steps
-sudo yum -y install python3
-ln -s /usr/bin/python3 python
+sudo yum -y install epel-release python3 python3-dnf dnf
+
+ln -sf /usr/bin/python3 python
 sudo mv python /usr/bin/python
 
 pip3 install --user ansible
