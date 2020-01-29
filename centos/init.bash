2fdebf630b6277e00fb11c06945c50454214b203
benton-gray <benton.gray@outlook.com>
2020-01-28 20:32:02 -0700

    Cleaning up vagrant box startup, moved provisioning and creation to packer_boxes repo

--- centos/init.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Init Provisioning Steps
-sudo yum -y install epel-release python3 python3-dnf dnf
-
-ln -sf /usr/bin/python3 python
-sudo mv python /usr/bin/python
-
-pip3 install --user ansible
